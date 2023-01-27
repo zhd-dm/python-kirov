71636255f6865128519b3c7b77b236c1540d903e
Demyan <dyo171717@gmail.com>
2023-01-27 21:17:43 +0300

    update: added getters and setters to EntityConfig class

--- src/fields/base_entity_config_generator.py
+++ src/fields/base_entity_config_generator.py
@@ -24,33 +24,66 @@ class OldEntityConfig():
         self.columns = columns
 
 class EntityConfig:
+    # protected _config
+    # private __entity_config
+
     def __init__(self, config: Dict[str, any]):
-        self.config = config
+        self._config = config
+        self.__entity_config: Dict[str, any] = self.config['entity_config']
         self.generate_fields()
 
     def generate_fields(self):
-        self.entity_config = self.config['entity_config']
-        self.parent_name = self.entity_config['parent_name']
-        self.entity_name = self.entity_config['entity_name']
-        self.type_method = self.entity_config['type_method']
-        self.params = self.entity_config['params']
-        self.fields = copy.deepcopy(self.config)
+        self._parent_name = self.__entity_config['parent_name']
+        self._entity_name = self.__entity_config['entity_name']
+        self._type_method = self.__entity_config['type_method']
+        self._params = self.__entity_config['params']
+        self._fields = copy.deepcopy(self._config)
         del self.fields['entity_config']
 
-    def get_config(self):
-        return self.config
+    @property
+    def config(self):
+        return self._config
 
-    def get_parent_name(self):
-        return self.parent_name
+    @config.setter
+    def config(self, v):
+        self._config = v
 
-    def get_entity_name(self):
-        return self.entity_name
+    @property
+    def parent_name(self):
+        return self._parent_name
 
-    def get_type_method(self):
-        return self.type_method
+    @parent_name.setter
+    def parent_name(self, v):
+        self._parent_name = v
+
+    @property
+    def entity_name(self):
+        return self._entity_name
+
+    @entity_name.setter
+    def entity_name(self, v):
+        self._entity_name = v
+
+    @property
+    def type_method(self):
+        return self._type_method
     
-    def get_params(self):
-        return self.params
+    @type_method.setter
+    def type_method(self, v):
+        self._type_method = v
 
-    def get_fields(self):
-        return self.fields
+    @property
+    def params(self):
+        return self._params
+
+    @params.setter
+    def params(self, v):
+        self._params = v
+
+    @property
+    def fields(self):
+        return self._fields
+
+    @fields.setter
+    def fields(self, v):
+        self._fields = v
