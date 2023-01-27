22763d148ee8e0d5c630fb922d82d766692320ca
Demyan <dyo171717@gmail.com>
2023-01-27 20:13:51 +0300

    fix: rename KEY_AND_VALUE const to DICT_FIELD_AND_DB_TYPE

--- src/fields/crm_productrow_list_fields.py
+++ src/fields/crm_productrow_list_fields.py
@@ -5,7 +5,8 @@ from fields.base_fields import ENTITY_BASE_KEYS, BASE_FIELDS_TO_DB_TYPES
 T_CRM_PRODUCTROW_LIST_FIELDS_KEYS = ['ID', 'OWNER_ID', 'PRODUCT_ID', 'PRODUCT_NAME', 'QUANTITY']
 T_CRM_PRODUCTROW_LIST_FIELDS_VALUES = [BASE_FIELDS_TO_DB_TYPES[i] for i in filter(lambda x: x in BASE_FIELDS_TO_DB_TYPES, T_CRM_PRODUCTROW_LIST_FIELDS_KEYS)]
 
-KEY_AND_VALUE = { T_CRM_PRODUCTROW_LIST_FIELDS_KEYS[i]: T_CRM_PRODUCTROW_LIST_FIELDS_VALUES[i] for i in range(len(T_CRM_PRODUCTROW_LIST_FIELDS_KEYS)) }
+DICT_FIELD_AND_DB_TYPE = { T_CRM_PRODUCTROW_LIST_FIELDS_KEYS[i]: T_CRM_PRODUCTROW_LIST_FIELDS_VALUES[i] for i in range(len(T_CRM_PRODUCTROW_LIST_FIELDS_KEYS)) }
+# [deal['ID'] for deal in deals]
 
 CRM_PRODUCTROW_LIST_CONFIG: Dict[str, any] = {
     'entity_config': {
@@ -13,9 +14,9 @@ CRM_PRODUCTROW_LIST_CONFIG: Dict[str, any] = {
         'entity_name': 'productrow',
         'type_method': 'list',
         'params': {
-            'filter': {'OWNER_TYPE': 'D', 'OWNER_ID': [deal['ID'] for deal in deals]}
+            'filter': {'OWNER_TYPE': 'D', 'OWNER_ID': 'custom'}
         },
         'keys': [item for sublist in (ENTITY_BASE_KEYS, T_CRM_PRODUCTROW_LIST_FIELDS_KEYS) for item in sublist]
     },
 }
-CRM_PRODUCTROW_LIST_CONFIG.update(KEY_AND_VALUE)
+CRM_PRODUCTROW_LIST_CONFIG.update(DICT_FIELD_AND_DB_TYPE)
