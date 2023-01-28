94c597bf63cd2c0a531ada0e5f5ced5b293e1780
Demyan <dyo171717@gmail.com>
2023-01-28 22:49:45 +0300

    fix: deleted deprecated and dead code

---
+++ Kirov/main.py
@@ -0,0 +1,21 @@
+import asyncio
+import time
+
+# Local imports
+
+from utils.utils import print_error
+
+async def main():
+
+    try:
+        print()
+        time.sleep(1)
+
+    except Exception as error:
+        print_error(error)
+
+    finally:
+        print()
+        print('finally')
+
+asyncio.run(main())
