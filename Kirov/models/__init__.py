9f6845350ce2364430e0e52e4e8968d78818ebcd
Demyan <dyo171717@gmail.com>
2023-01-30 22:15:44 +0300

    refactor: unit-tests moving to other directory "tests"

--- Kirov/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.ext.declarative import declarative_base
-
-
-from models.user.test_user_model import TestUserModel
-from models.user.user_model import User
-
-
-Base = declarative_base()
---
+++ Kirov/tests/__init__.py
@@ -0,0 +1,8 @@
+from sqlalchemy.ext.declarative import declarative_base
+
+
+from tests.user.test_user_model import TestUserModel
+from tests.user.user_model import User
+
+
+Base = declarative_base()
