63a97d29ca0a71e2654753d7a337d912f7972297
Demyan <dyo171717@gmail.com>
2023-02-02 20:48:37 +0300

    fix: bugs - #3, #4, #5

--- Kirov/fields/base_fields_types.py
+++ Kirov/fields/base_fields_types.py
@@ -4,11 +4,13 @@ from typing import Dict, List, Union
 # ================== Базовые типы для конфига ==================
 #
 
+T_PARENT_NAME = str
+T_ENTITY_NAME = str
 T_CALL_METHOD = str
 T_PARAMS = Dict[str, List[str]]
 T_KEYS = List[str]
 T_ENUMS = Dict[str, List[any]]
 T_PRIMARY_KEY = str
 T_FIELDS = Dict[str, str]
-T_ENTITY_CONFIG = Dict[str, Union[ T_CALL_METHOD, T_PARAMS, T_KEYS, T_ENUMS, T_PRIMARY_KEY ]]
+T_ENTITY_CONFIG = Dict[str, Union[ T_PARENT_NAME, T_ENTITY_NAME, T_CALL_METHOD, T_PARAMS, T_KEYS, T_ENUMS, T_PRIMARY_KEY ]]
 T_ENTITY_CONFIG_WITH_FIELDS = Dict[str, Union[ T_ENTITY_CONFIG, T_FIELDS ]]
