394388e50726fa73b114cf7638cacce5376d6dd7
Demyan <64752156+zhd-dm@users.noreply.github.com>
2023-02-02 22:00:10 +0300

    feat: added check errors in insert values to table (#14)

--- Kirov/tables/base_table.py
+++ Kirov/tables/base_table.py
@@ -18,13 +18,15 @@ class BaseTable:
     def __init__(self, engine: Engine, metadata: MetaData, entity_config: BaseConfig):
         self.__metadata = metadata
         self.__engine = engine
-        self.__entity_config = entity_config
-        self.__columns = BaseColumns(self.__entity_config).column_list
+        self.__connection = engine.connect()
 
+        self.__entity_config = entity_config
+
+        self.__columns = BaseColumns(self.__entity_config).column_list
         self.__tablename__ = self.__entity_config.entity_name
         self.__table = Table(self.tablename, self.__metadata, *self.__columns)
 
-        self.__connection = engine.connect()
+        self.__count_records = 0
 
     def _drop_and_create(self):
         self.__drop()
@@ -35,13 +37,15 @@ class BaseTable:
             element = self.__empty_str_to_none(key_dict_to_lower(element))
             
             try:
-                element = { k: v for k, v in element.items() if k in self.__entity_config.keys_lower }
+                element = { k: v for k, v in element .items() if k in self.__entity_config.keys_lower }
                 self.__connection.execute(self.__table.insert().values(**element))
+                self.__count_records += 1
                 print_success(f'Запись успешно добавлена в таблицу {self.tablename}')
         
             except Exception as error:
-                print_error(f'Не удалось добавить запись в таблицу {self.tablename}. Ошибка: {str(error)}')
+                print_error(f'Не удалось добавить запись в таблицу {self.tablename}. Ошибка: {error}')
         
+        self.__check_all_data_added_to_table(data)
         self.__connection.close()
 
     def __create(self):
@@ -61,3 +65,9 @@ class BaseTable:
             data['closedate'] = None
 
         return data
+
+    def __check_all_data_added_to_table(self, data):
+        if self.__count_records != data.__len__():
+            print_error(f'{data.__len__() - self.__count_records} записи не было занесено в таблицу')
+        else:
+            print_success(f'OK Все записи были успешно добавлены в таблицу {self.tablename}')
