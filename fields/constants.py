c9eb2b06d74e2fafd1ae4b99f3d0e09d063f587a
Demyan <dyo171717@gmail.com>
2023-02-16 21:48:09 +0300

    feat: get methods list from Google Sheets

--- fields/constants.py
+++ fields/constants.py
@@ -7,26 +7,6 @@ from fields.types import T_KEYS
 ENTITY_CONFIG_KEYS: T_KEYS = ['parent_name', 'entity_name', 'type_method', 'params', 'enums', 'primary_key', 'keys']
 
 #
-# REFACTOR:
-# Подумать как получать из Google Sheets
-#
-
-BITRIX_METHODS = [
-    'crm.deal.list',
-    'catalog.catalog.list',
-    'catalog.document.element.list',
-    'catalog.document.list',
-    'catalog.product.offer.list',
-    'catalog.product.sku.list',
-    'catalog.store.list',
-    'catalog.storeproduct.list',
-    'crm.product.list',
-    'crm.productrow.list',
-    'crm.company.list',
-    'user.get'
-]
-
-#
 # ================== Метод получения списка кастомных параметров запроса ==================
 #
 
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import asyncio
 from typing import List, Union
 
+from env import DATALENS_SERVER_SETTINGS
+from google_sheets.google_sheet import GoogleSheet
+from google_sheets.constants import RANGE_METHODS_NAMES
 from generate_entities import GenerateEntities
-from utils import Settings
-
-from fields.constants import BITRIX_METHODS
+from utils import Settings, get_list_by_index_of_matrix
 
 
 #
@@ -17,9 +18,13 @@ from fields.constants import BITRIX_METHODS
 async def main():
 
     settings = Settings()
-    await GenerateEntities(settings, BITRIX_METHODS)._generate_entities()
+    bitrix_methods = get_list_by_index_of_matrix(0, GoogleSheet()._get_range_values(RANGE_METHODS_NAMES))
+    await GenerateEntities(settings, bitrix_methods)._generate_entities()
     settings.engine.pool.dispose()
 
+    # settings = Settings(SERVER_SETTINGS)
+    # connection = settings.connection
+    # settings.engine.pool.dispose()
 
 if __name__ == '__main__':
     asyncio.run(main())
