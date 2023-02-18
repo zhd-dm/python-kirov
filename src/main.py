c24301cb0179e55dc92e9d004a9ebc3f0c47e643
Demyan <dyo171717@gmail.com>
2023-02-18 16:49:18 +0300

    feat: run script every hour

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import asyncio
+import time
 from typing import List, Union
 
 from env import DATALENS_SERVER_CONNECTION
@@ -6,7 +7,8 @@ from connectors.db_connector import DBConnector
 from google_sheets.google_sheet import GoogleSheet
 from google_sheets.config.constants import RANGE_METHODS_NAMES
 from data_generators.generate_entities import GenerateEntities
-from utils.mapping import get_list_by_index_of_matrix
+from utils.mapping import get_list_by_index_of_matrix, print_now_date
+from config.constants import HOUR
 
 
 #
@@ -16,12 +18,17 @@ from utils.mapping import get_list_by_index_of_matrix
 # Продумать разделение на роли
 #
 
-async def main():
-
-    connector = DBConnector()
+async def begin():
+    print_now_date('Текущее время сервера')
+    connector = DBConnector(DATALENS_SERVER_CONNECTION)
     bitrix_methods = get_list_by_index_of_matrix(0, GoogleSheet()._get_range_values(RANGE_METHODS_NAMES))
     await GenerateEntities(connector, bitrix_methods)._generate_entities()
     connector.engine.pool.dispose()
+
+async def main():
+    while True:
+        await begin()
+        time.sleep(HOUR)
 
 if __name__ == '__main__':
     asyncio.run(main())
