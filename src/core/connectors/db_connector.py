57dfde6026b3aa3a4eeac97eb7771ef1feb8d043
Demyan <dyo171717@gmail.com>
2023-02-21 20:26:15 +0300

    refactor: вынесение ненужной логики из класса DBConnector в другие классы BXConnector и GSConnector

--- src/core/connectors/db_connector.py
+++ src/core/connectors/db_connector.py
@@ -1,13 +1,14 @@
 from sqlalchemy import create_engine, MetaData
 
 
-from env import webhook, DEFAULT_CONNECTION, SPREADSHEET_ID
+from env import LOCAL_DEFAULT_CONNECTION
 
 from config.types import T_CONNECTION
 
 
 class DBConnector:
-    def __init__(self, connection: T_CONNECTION = DEFAULT_CONNECTION):
+
+    def __init__(self, connection: T_CONNECTION = LOCAL_DEFAULT_CONNECTION):
         self.__host = connection['host']
         self.__port = connection['port']
         self.__db = connection['db']
@@ -15,16 +16,8 @@ class DBConnector:
         self.__password = connection['password']
 
     @property
-    def webhook(self):
-        return webhook
-
-    @property
     def db_url(self):
         return f'postgresql://{self.__user}:{self.__password}@{self.__host}:{self.__port}/{self.__db}'
-
-    @property
-    def spreadsheet_id(self):
-        return SPREADSHEET_ID
 
     @property
     def engine(self):
---
+++ src/core/connectors/gs_connector.py
@@ -0,0 +1,8 @@
+from env import SPREADSHEET_ID
+
+
+class GSConnector:
+    
+    @property
+    def spreadsheet_id(self):
+        return SPREADSHEET_ID
