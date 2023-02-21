cb9bc76a80b54f5f65750383e98145a2eaaec0d8
Demyan <dyo171717@gmail.com>
2023-02-21 20:35:13 +0300

    refactor: вынос метода получения данных из BX в отдельный класс

---
+++ src/core/api_calls/bx_api.py
@@ -0,0 +1,22 @@
+from typing import Union, Dict, List
+
+from fast_bitrix24 import BitrixAsync
+
+
+from entity_configs.entity_config import EntityConfig
+from core.connectors.bx_connector import BXConnector
+from utils.mapping import print_info
+
+
+class BXApi:
+
+    async def _get_bx_data(self, req_obj: EntityConfig) -> Union[List, Dict]:
+        webhook = BXConnector().webhook
+        bx = BitrixAsync(webhook, False)
+        method = f'{req_obj.parent_name}.{req_obj.entity_name}.{req_obj.type_method}' if req_obj.parent_name else f'{req_obj.entity_name}.{req_obj.type_method}'
+        print_info(f'Method name -> {method}')
+
+        return await bx.get_all(
+            method,
+            params = req_obj.params
+        )
