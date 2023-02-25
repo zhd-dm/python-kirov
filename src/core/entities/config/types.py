cefcee4c222093c8eb69f9eefeadf6d3f460b7e5
Demyan <dyo171717@gmail.com>
2023-02-25 15:08:46 +0300

    delete: удалены первые попытки рефакторинга

--- src/core/entities/config/types.py
+++
@@ -1,9 +0,0 @@
-from typing import Union, List, Dict
-
-# ================== Базовые типы для конфига сущности ==================
-
-T_ENTITY_METHOD = str
-T_ENTITY_PARAMS = Dict[str, any]
-T_ENTITY_DELIVERED_FIELDS = List[str]
-
-T_ENTITY = Dict[str, Union[T_ENTITY_METHOD, T_ENTITY_PARAMS, T_ENTITY_DELIVERED_FIELDS]]
